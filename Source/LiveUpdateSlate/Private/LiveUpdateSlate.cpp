d41bc7da7d637048094be6b655e1dad1a070a7d6
Sythenz <alessafur@protonmail.com>
2022-07-11 01:56:52 +0200

    Added Live Update for Slate

---
+++ Source/LiveUpdateSlate/Private/LiveUpdateSlate.cpp
@@ -0,0 +1,63 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LiveUpdateSlate.h"
+#include <Windows/LiveCoding/Public/ILiveCodingModule.h>
+#include <MainFrame/Public/Interfaces/IMainFrameModule.h>
+#include <Framework/Docking/TabManager.h>
+#include <LiveUpdateSlateSettings.h>
+#include <ISettingsModule.h>
+
+#define LOCTEXT_NAMESPACE "FLiveUpdateSlateModule"
+
+void FLiveUpdateSlateModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings");
+	if (SettingsModule != nullptr)
+	{
+		SettingsModule->RegisterSettings("Editor", "Plugins", "Slate Update for Live++",
+			LOCTEXT("LiveUpdateSlateSettingsName", "Slate Update for Live++"),
+			LOCTEXT("LiveUpdateSlateSettingsDescription", "Settings for Slate Update for Live++"),
+			GetMutableDefault< ULiveUpdateSlateSettings >()
+		);
+	}
+
+#if WITH_LIVE_CODING
+	ILiveCodingModule* LiveCoding = FModuleManager::LoadModulePtr<ILiveCodingModule>(LIVE_CODING_MODULE_NAME);
+	const bool LiveCodingEnabled = LiveCoding != nullptr && LiveCoding->IsEnabledForSession();
+
+
+	if (LiveCodingEnabled)
+	{
+		LiveCoding->GetOnPatchCompleteDelegate().AddLambda([] {
+
+			ULiveUpdateSlateSettings* Settings = GetMutableDefault<ULiveUpdateSlateSettings>();
+
+			//Don't rebuild slate if the plugin is not enabled
+			if (!Settings->bEnableLiveUpdateForSlate) return;
+
+			//Save all open tabs
+			FGlobalTabmanager::Get()->SaveAllVisualState();
+
+			//Flush our config files
+			const bool bRead = true;
+			GConfig->Flush(bRead, GEditorLayoutIni);
+
+			//Request the editor to rebuild Slate
+			IMainFrameModule& MainFrameModule = FModuleManager::LoadModuleChecked<IMainFrameModule>(TEXT("MainFrame"));
+			MainFrameModule.RecreateDefaultMainFrame(false, false);
+		});
+	}
+#endif
+
+}
+
+void FLiveUpdateSlateModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FLiveUpdateSlateModule, LiveUpdateSlate)
---
+++ Source/LiveUpdateSlate/Public/LiveUpdateSlateSettings.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "LiveUpdateSlateSettings.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS(config = EditorPerProjectUserSettings)
+class LIVEUPDATESLATE_API ULiveUpdateSlateSettings : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(Config, EditAnywhere, Category = RHI, meta = (DisplayName = "Enable Live Update for Slate", ToolTip = "Refreshes the Editor's Slate Layout when Live Coding patches are complete."))
+	bool bEnableLiveUpdateForSlate = true;
+};
